1e01de4a0f8c85b9138a6433099d4b90f8b29b61
YoussefBenLahouel <youssef.benlahouel@esprit.tn>
2024-09-30 14:37:51 +0100

    commetest

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -18,7 +18,10 @@ import javax.persistence.TemporalType;
 @Table(name = "T_USER")
 public class User implements Serializable {
 
+	//test
 
+
+	
 	private static final long serialVersionUID = 1L;
 
 	@Id
